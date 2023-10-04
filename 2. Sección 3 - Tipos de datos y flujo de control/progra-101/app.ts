fc2aa7c07039077824a2305752bb334c16fe4c7e
Alquimista-07 <jdca.07@gmail.com>
2023-10-04 15:51:58 -0500

    29. Tarea - If else

--- 2. Sección 3 - Tipos de datos y flujo de control/progra-101/app.ts
+++ 2. Sección 3 - Tipos de datos y flujo de control/progra-101/app.ts
@@ -22,6 +22,8 @@ let number4: number = 20.3;
 
 console.log(number3 + number4);
 
+console.log('----------------------------------------------------------------------');
+
 // Boolean
 
 let isActive: boolean = true;
@@ -31,6 +33,8 @@ let hasKids: boolean = true;
 console.log('isActive', isActive);
 console.log('isRunning', isRunning);
 console.log('hasKids', hasKids);
+
+console.log('----------------------------------------------------------------------');
 
 //--------------------------------------------------------------------------
 //                        CONTROL STRUCTURES
@@ -48,13 +52,46 @@ if ( isTired ) {
 
 console.log('Fin de programa');
 
+console.log('----------------------------------------------------------------------');
+
+console.log('Inicio de programa');
 // IF - ELSE Anidados
 export let grade: number = 90;
 
 if( grade >= 60 ){
     console.log('El alumno aprueba la clase');
 } else if( grade > 50 ){
-        console.log('Por favor estudie más');
+    console.log('Por favor estudie más');
 }else {
     console.log('El alumno NO aprueba la clase');
 }
+console.log('Fin de programa');
+
+console.log('----------------------------------------------------------------------');
+
+//--------------------------------------------------------------------------
+//                        TAREA IF - ELSE
+//--------------------------------------------------------------------------
+
+console.log('Inicio de programa');
+
+export let grade2: number = 80;
+let gradeLetter: string;
+
+if ( grade2 >= 90 ) {
+    gradeLetter = 'A';
+} else if( grade2 >= 80 ) {
+    gradeLetter = 'B';
+} else if( grade2 >= 70 ) {
+    gradeLetter = 'C';
+} else if( grade2 >= 60 ){
+    gradeLetter = 'D';
+} else {
+    gradeLetter = 'F';
+}
+
+console.log('La nota del alumno es: ', gradeLetter);
+
+console.log('Fin de programa');
+
+console.log('----------------------------------------------------------------------');
