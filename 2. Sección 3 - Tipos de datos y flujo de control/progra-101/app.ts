8f444ddaa3f4cffe7a6017e9d74fc33cd6482af4
Alquimista-07 <jdca.07@gmail.com>
2023-10-04 14:44:04 -0500

    Reproducir 25. Continuación de ejemplo

--- 2. Sección 3 - Tipos de datos y flujo de control/progra-101/app.ts
+++ 2. Sección 3 - Tipos de datos y flujo de control/progra-101/app.ts
@@ -1,5 +1,8 @@
+//--------------------------------------------------------------------------
+//                        BASIC TYPES
+//--------------------------------------------------------------------------
 // Strings
-let firstName: string = 'Fernando';
+export let firstName: string = 'Fernando';
 let lastName : string = 'Herrera';
 
 // console.log( firstName, lastName );
@@ -7,7 +10,24 @@ let fullName: string = firstName + ' ' + lastName;
 
 console.log(fullName);
 
+// Number
+
 let number1: number = 10;
 let number2: number = 20;
 
 console.log(number1 - number2);
+
+let number3: number = 10.5;
+let number4: number = 20.3;
+
+console.log(number3 + number4);
+
+// Boolean
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+console.log('isActive', isActive);
+console.log('isRunning', isRunning);
+console.log('hasKids', hasKids);
