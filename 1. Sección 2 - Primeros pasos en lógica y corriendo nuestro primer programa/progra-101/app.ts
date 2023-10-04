e428acf0ff1cc3a58105fb75eabbbf7f64b46357
Alquimista-07 <jdca.07@gmail.com>
2023-10-04 11:20:10 -0500

    15. Breve introducción a funciones

--- 1. Sección 2 - Primeros pasos en lógica y corriendo nuestro primer programa/progra-101/app.ts
+++ 1. Sección 2 - Primeros pasos en lógica y corriendo nuestro primer programa/progra-101/app.ts
@@ -1,5 +1,6 @@
 let numberOfLines = 1
 
+// Ejercicio #1
 console.log('Línea #', numberOfLines);
 
 numberOfLines++;
@@ -11,4 +12,22 @@ console.log('Línea #', numberOfLines);
 numberOfLines += 1;
 console.log('Línea #', numberOfLines);
 
+console.log('----------------------------------------');
 
+// Ejercicio #2
+let numberOfLines2 = 1
+
+function increaseLineNumber() {
+    numberOfLines2+=3;
+}
+
+console.log('Línea #', numberOfLines2);
+
+increaseLineNumber();
+console.log('Línea #', numberOfLines2);
+
+increaseLineNumber();
+console.log('Línea #', numberOfLines2);
+
+increaseLineNumber();
+console.log('Línea #', numberOfLines2);
