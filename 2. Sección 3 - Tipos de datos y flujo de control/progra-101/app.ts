b3fa265b8349520f760bfec44685dd12a756baeb
Alquimista-07 <jdca.07@gmail.com>
2023-10-04 14:27:23 -0500

    24. Ejemplo práctico de tipos de datos

--- 2. Sección 3 - Tipos de datos y flujo de control/progra-101/app.ts
+++ 2. Sección 3 - Tipos de datos y flujo de control/progra-101/app.ts
@@ -1,3 +1,13 @@
+// Strings
+let firstName: string = 'Fernando';
+let lastName : string = 'Herrera';
 
-console.log('Hola Mundo');
+// console.log( firstName, lastName );
+let fullName: string = firstName + ' ' + lastName;
 
+console.log(fullName);
+
+let number1: number = 10;
+let number2: number = 20;
+
+console.log(number1 - number2);
