520fb0bedfb18064733da7e9f8694cf66b6615b2
Alquimista-07 <jdca.07@gmail.com>
2023-10-05 09:13:45 -0500

    42. Ejemplo de función

--- 3. Sección 4 - Funciones y arreglos/progra-101/app.ts
+++ 3. Sección 4 - Funciones y arreglos/progra-101/app.ts
@@ -10,3 +10,19 @@ export function greet( name: string = 'Mundo!!!' ) {
 let firstName = 'Fernando';
 
 greet( firstName );
+
+console.log('--------------------------------------------------------------------');
+
+//--------------------------------------------------------------------------
+//                        EJEMPLO DE FUNCIÓN
+//--------------------------------------------------------------------------
+function addTwoNumbers( num1: number, num2: number ): number {
+    return num1 + num2;
+}
+
+ let total: number = addTwoNumbers( 10, 20 );
+
+ console.log('Total: ' + total);
+
+ console.log('--------------------------------------------------------------------');
+ 
