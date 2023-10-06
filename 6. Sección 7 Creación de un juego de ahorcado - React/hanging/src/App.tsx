9226e3626d8efaf447dc321fab86553f21aa205c
Alquimista-07 <jdca.07@gmail.com>
2023-10-06 14:49:39 -0500

    81. Recursos del ahorcado

--- 6. Sección 7 Creación de un juego de ahorcado - React/hanging/src/App.tsx
+++ 6. Sección 7 Creación de un juego de ahorcado - React/hanging/src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css';
+// Importamos el arreglo de letras
+import { letters } from './helpers/letters';
+
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='App'>
-      <h1>Hola Mundo!</h1>
+    <div className="App">
+      {/* Imágenes */}
+      <h3>Imágen del Juego</h3>
+
+      {/* Palabra oculta */}
+      <h3>_ _ _ _ _ _ _ _ _ _</h3>
+
+      {/* Contador de intentos */}
+      <h3>Intentos: 0</h3>
+      
+      {/* Botones de letras */}
+      {
+        letters.map( (letter) => (
+          <button 
+            key={ letter }>
+              { letter }
+          </button>
+        ))
+      }
     </div>
-  )
+  );
 }
 
 export default App;
